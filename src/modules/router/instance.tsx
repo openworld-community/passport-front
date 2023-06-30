dcb7c93d210c9d0e6f18ead260661e58ba0facc4
Konstantin <konstantin.miheev@sensearena.com>
2023-06-30 12:59:40 +0200

    fixed router basename

--- src/modules/router/instance.tsx
+++ src/modules/router/instance.tsx
@@ -5,24 +5,27 @@ import { PasswordForget } from '@m/password-forget/PasswordForget';
 import { SignUpLayout } from '@m/sign-up/SignUpLayout';
 import { createBrowserRouter } from 'react-router-dom';
 
-export const routerInstance = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'sign-up',
-        element: <SignUpLayout />,
-      },
-      {
-        path: 'login',
-        element: <LoginLayout />,
-      },
-      {
-        path: 'password-forget',
-        element: <PasswordForget />,
-      },
-    ],
-  },
-]);
+export const routerInstance = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: 'sign-up',
+          element: <SignUpLayout />,
+        },
+        {
+          path: 'login',
+          element: <LoginLayout />,
+        },
+        {
+          path: 'password-forget',
+          element: <PasswordForget />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
