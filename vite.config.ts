83f5303671c981fa8bdcfa40c8d339db596be6cf
Konstantin <konstantin.miheev@sensearena.com>
2023-07-03 08:47:59 +0200

    vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       src: resolve(__dirname, './src'),
@@ -13,4 +13,5 @@ export default defineConfig(() => ({
     },
   },
   plugins: [react()],
+  base: mode === 'development' ? '/' : '/passport-front/',
 }));
