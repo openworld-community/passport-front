0cabb7a56aa6f7b1e43a7cb6347a2d5b658bfcad
Kon <whynunu@why.com>
2023-07-06 15:03:26 +0200

    added DiscordIcon and reuse it

--- src/modules/login/LoginLayout.tsx
+++ src/modules/login/LoginLayout.tsx
@@ -5,6 +5,7 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { Button, Checkbox, FormControlLabel, IconButton, InputAdornment, Stack, TextField, Typography } from '@mui/material';
 import Link from '@mui/material/Link';
+import { DiscordIcon } from '@ui/icons/DiscordIcon';
 import { useFormik } from 'formik';
 import { useState } from 'react';
 import * as Yup from 'yup';
@@ -84,14 +85,7 @@ export const LoginLayout = () => {
       </Button>
       <Stack direction="row" spacing={1} marginTop={'30px'}>
         <Button variant="outlined">
-          {
-            <svg viewBox="0 0 48 48" width="24px" height="24px">
-              <path
-                fill="#556cd6"
-                d="M40,12c0,0-4.585-3.588-10-4l-0.488,0.976C34.408,10.174,36.654,11.891,39,14c-4.045-2.065-8.039-4-15-4s-10.955,1.935-15,4c2.346-2.109,5.018-4.015,9.488-5.024L18,8c-5.681,0.537-10,4-10,4s-5.121,7.425-6,22c5.162,5.953,13,6,13,6l1.639-2.185C13.857,36.848,10.715,35.121,8,32c3.238,2.45,8.125,5,16,5s12.762-2.55,16-5c-2.715,3.121-5.857,4.848-8.639,5.815L33,40c0,0,7.838-0.047,13-6C45.121,19.425,40,12,40,12z M17.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C21,28.209,19.433,30,17.5,30z M30.5,30c-1.933,0-3.5-1.791-3.5-4c0-2.209,1.567-4,3.5-4s3.5,1.791,3.5,4C34,28.209,32.433,30,30.5,30z"
-              />
-            </svg>
-          }
+          <DiscordIcon fill="#556cd6" />
         </Button>
         <Button variant="outlined">{<TelegramIcon />}</Button>
       </Stack>
