626435782b2882d35916fcd2ee560eaf77d891fa
Anna Ardinartceva <k31082002@gmail.com>
2023-07-11 15:48:44 +0300

    PasswordReset

--- src/modules/router/instance.tsx
+++ src/modules/router/instance.tsx
@@ -2,6 +2,7 @@ import { RootLayout } from '@m/base/RootLayout';
 import { ErrorPage } from '@m/errors/ErrorPage';
 import { LoginLayout } from '@m/login/LoginLayout';
 import { PasswordForget } from '@m/password-forget/PasswordForget';
+import { PasswordReset } from '@m/password-reset/PasswordReset';
 import { SignUpLayout } from '@m/sign-up/SignUpLayout';
 import { createHashRouter } from 'react-router-dom';
 
@@ -22,6 +23,10 @@ export const routerInstance = createHashRouter([
       {
         path: 'password-forget',
         element: <PasswordForget />,
+      },
+      {
+        path:'password-reset',
+        element:<PasswordReset/>
       },
     ],
   },
