7791ecee94c50ab0f7acbc95f7e35b748547cf28
Kon <whynunu@why.com>
2023-07-17 13:08:31 +0200

    added fetcher with refresh token and local store api

--- src/core/data/fetcher.ts
+++ src/core/data/fetcher.ts
@@ -1,5 +1,34 @@
+import { LoginResponse } from '@core/contracts';
+import { LS, LSKeys } from '@core/local-store';
 import ky from 'ky';
+
+const { access_token, refresh_token } = LS.getItem(LSKeys.AuthInfo, null) ?? {};
 
 export const API = ky.create({
   prefixUrl: import.meta.env.VITE_API_URL,
+  headers: access_token
+    ? {
+        Authorization: `Bearer ${access_token}`,
+      }
+    : {},
+  hooks: {
+    afterResponse: [
+      async (request, _options, response) => {
+        if (response.status === 403) {
+          // Get a fresh token
+          const { token } = await API.post('refreshtoken', {
+            json: {
+              refresh_token,
+            },
+          }).json<LoginResponse>();
+
+          LS.setItem(LSKeys.AuthInfo, token);
+
+          request.headers.set('Authorization', `Bearer ${token.access_token}`);
+
+          return ky(request);
+        }
+      },
+    ],
+  },
 });
---
+++ src/core/local-store/index.ts
@@ -0,0 +1,38 @@
+import { TokenData } from '@core/contracts';
+
+export enum LSKeys {
+  AuthInfo = 'auth_info',
+}
+
+export interface LSData {
+  [LSKeys.AuthInfo]: TokenData | null;
+}
+
+const getItem = <K extends LSKeys>(key: K, defaultValue: LSData[K]): LSData[K] => {
+  try {
+    const v = localStorage.getItem(key);
+    return v ? JSON.parse(v) : defaultValue;
+  } catch {
+    return defaultValue;
+  }
+};
+const setItem = <K extends LSKeys>(key: K, value: LSData[K]) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error(error);
+  }
+};
+const deleteItem = <K extends LSKeys>(key: K) => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const LS = {
+  getItem,
+  setItem,
+  deleteItem,
+};
